f1f645635d305da6e7ce6dfcc1c2b18f1741924f
krzysztof-galazka <kgalazka@vp.pl>
2020-03-11 23:59:57 +0100

    Task 6.2 - adding calculator test

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,9 +1,38 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
     public static void main(String[] args) {
 
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowaniaa");
+        //user test
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
 
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")){
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        //calculator test
+        Calculator calculator = new Calculator();
+        int addingTest = calculator.add(4,2);
+
+        if (addingTest == 6){
+            System.out.println("Adding test OK");
+        } else {
+            System.out.println("Error in adding");
+        }
+
+        int substractTest = calculator.substract(9,4);
+
+        if (substractTest == 2){
+            System.out.println("Substract test OK");
+        } else {
+            System.out.println("Error in substract");
+        }
     }
 }
