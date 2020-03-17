058eae3f960485474c4324df765eccddf8b686e4
krzysztof-galazka <kgalazka@vp.pl>
2020-03-17 08:14:36 +0100

    Problem z junit

---
+++ kodilla-testing/src/main/java/com/kodilla/testing/collection/OddNumbersExterminator.java
@@ -0,0 +1,16 @@
+package com.kodilla.testing.collection;
+
+import java.util.ArrayList;
+
+public class OddNumbersExterminator {
+    ArrayList <Integer> evenNumbers = new ArrayList<>();
+
+    for (Integer number:numbers)
+
+    {
+        if (number % 2 = 0) {
+            evenNumbers.add(number);
+        }
+        return evenNumbers:
+    }
+}
