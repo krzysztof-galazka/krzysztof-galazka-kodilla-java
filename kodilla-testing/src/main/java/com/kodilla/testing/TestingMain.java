2a5fa442cae6f00616820f08a150fb7a22f7a8b0
krzysztof-galazka <kgalazka@vp.pl>
2020-03-17 19:55:57 +0100

    Task 6.4 - shape collector

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -6,16 +6,16 @@ import com.kodilla.testing.user.SimpleUser;
 public class TestingMain {
     public static void main(String[] args) {
 
-        //user test
-        SimpleUser simpleUser = new SimpleUser("theForumUser");
-
-        String result = simpleUser.getUsername();
-
-        if (result.equals("theForumUser")){
-            System.out.println("test OK");
-        } else {
-            System.out.println("Error!");
-        }
+//        //user test
+//        SimpleUser simpleUser = new SimpleUser("theForumUser");
+//
+//        String result = simpleUser.getUsername();
+//
+//        if (result.equals("theForumUser")){
+//            System.out.println("test OK");
+//        } else {
+//            System.out.println("Error!");
+//        }
 
         //calculator test
         Calculator calculator = new Calculator();
