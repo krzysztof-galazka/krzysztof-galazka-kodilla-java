6b75f56bea8876093e346475b1f4edeaa94395bb
krzysztof-galazka <kgalazka@vp.pl>
2020-03-24 21:29:38 +0100

    Changes in BoadrdTestSuite

--- kodilla-stream/src/test/java/stream/portfolio/BoardTestSuite.java
+++ kodilla-stream/src/test/java/stream/portfolio/BoardTestSuite.java
@@ -8,6 +8,7 @@ import portfolio.TaskList;
 import portfolio.User;
 
 import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
