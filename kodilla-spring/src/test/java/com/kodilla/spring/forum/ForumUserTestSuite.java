4098557bfee3b82942a8c61781d546bf785a82ea
krzysztof-galazka <kgalazka@vp.pl>
2020-05-26 07:55:41 +0200

    Task 14.1 - forum user & creating first spring tests

---
+++ kodilla-spring/src/test/java/com/kodilla/spring/forum/ForumUserTestSuite.java
@@ -0,0 +1,28 @@
+package com.kodilla.spring.forum;
+
+
+import javafx.beans.binding.When;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ForumUserTestSuite {
+
+    @Test
+    public void testUsername(){
+        //Given
+        ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring.forum");
+        ForumUser forumUser = context.getBean(ForumUser.class);
+        //When
+        String username = forumUser.username;
+        //Then
+        Assert.assertEquals("John Smith", username);
+    }
+
+}
