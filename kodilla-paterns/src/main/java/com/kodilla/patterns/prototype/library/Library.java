6b2bf38b8e81a5ef8204758c32f299c55c8c0288
krzysztof-galazka <kgalazka@vp.pl>
2020-05-31 22:33:57 +0200

    Task 15.2 - Prototype - library

---
+++ kodilla-paterns/src/main/java/com/kodilla/patterns/prototype/library/Library.java
@@ -0,0 +1,51 @@
+package com.kodilla.patterns.prototype.library;
+
+import com.kodilla.patterns.prototype.TasksList;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public final class Library {
+    private String name;
+    private Set<Book> books = new HashSet<>();
+
+    public Library(final String name) {
+        this.name = name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Set<Book> getBooks() {
+        return books;
+    }
+
+    @Override
+    public String toString() {
+        String s = "Library [" + name + "]\n";
+        for (Book book:books) {
+            s = s + book.toString() + "\n";
+        }
+        return s;
+    }
+
+    public Library shallowCopy() throws CloneNotSupportedException{
+        return (Library)super.clone();
+    }
+
+    public Library deepCopy() throws CloneNotSupportedException{
+        Library clonedLibrary = (Library)super.clone();
+        clonedLibrary.books = new HashSet<>();
+        for (Book book : books){
+            Book clonedBook = new Book(book.getTitle(), book.getTitle(), book.getPublicationDate());
+            clonedLibrary.getBooks().add(clonedBook);
+        }
+        return clonedLibrary;
+    }
+}
