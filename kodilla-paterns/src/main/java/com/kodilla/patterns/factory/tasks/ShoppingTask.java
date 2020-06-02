a33dd64b90a6a7b3a86ed78b497324aec7e49ca4
krzysztof-galazka <kgalazka@vp.pl>
2020-06-02 02:05:49 +0200

    Task 15.4 - Factory - Tasks

---
+++ kodilla-paterns/src/main/java/com/kodilla/patterns/factory/tasks/ShoppingTask.java
@@ -0,0 +1,37 @@
+package com.kodilla.patterns.factory.tasks;
+
+public class ShoppingTask implements Task {
+
+    private final String taskName;
+    private final String whatToBuy;
+    private final double quantity;
+
+    public ShoppingTask(String taskName, String whatToBuy, double quantity) {
+        this.taskName = taskName;
+        this.whatToBuy = whatToBuy;
+        this.quantity = quantity;
+    }
+
+    @Override
+    public String getTaskName() {
+        return taskName;
+    }
+
+    public String getWhatToBuy() {
+        return whatToBuy;
+    }
+
+    public double getQuantity() {
+        return quantity;
+    }
+
+    @Override
+    public void executeTask() {
+
+    }
+
+    @Override
+    public boolean isTaskExecuted() {
+        return false;
+    }
+}
