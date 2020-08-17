56bd382da448e3ea8ab2cab2f32e120f6b59a6ef
krzysztof-galazka <kgalazka@vp.pl>
2020-08-18 00:44:32 +0200

    Excercise 24.1 - facade

---
+++ kodilla-project/src/main/java/fsdf.java
@@ -0,0 +1,21 @@
+//import java.awt.print.Book;
+//import java.util.ArrayList;
+//import java.util.HashMap;
+//import java.util.List;
+//
+//public class Test {
+//    public static void main(String[] args) {
+//        List<Book> books = new ArrayList<>();
+//        books.add(new Book("Bruce Eckel", "Thinking in Java", 2004));
+//        books.add(new Book("Scott Oaks", "Java Performance", 2009));
+//        books.add(new Book("Maurice Naftalin", "Java generics and collections", 2002));
+//
+//        Map<> booksMap = new HashMap<>();
+//        for (Book book : books) {
+//            booksMap.{{codeGap:3}}(book.getTitle(), book);
+//        }
+//
+//        {{codeGap:4}}<{{codeGap:5}}> titles = booksMap.keySet();
+//        System.out.println("All titles: " + titles);
+//    }
+//}
